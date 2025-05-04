2a0da7a294ba2e196d811c22e9bc49828c4b0280
SebaPerez90 <sebastianalbertoperez90@gmail.com>
2025-05-03 22:02:52 -0300

    updates

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,12 +11,12 @@ const Hero = () => {
       id='hero-section'
       className='py-28 w-full h-dvh relative bg-white dark:bg-dark-deep'>
       <div className='flex flex-col gap-12 items-center justify-center h-full'>
-        <header className='flex flex-col items-center justify-center gap-10 text-center w-[80%] md:w-[40em] lg:w-[75em] mx-auto'>
+        <header className='flex flex-col items-center justify-center gap-10 text-center w-[90%] lg:w-[60em] xl:w-[75em] mx-auto'>
           <TextGenerateEffect
             words={t('landing.title')}
-            className='text-4xl sm:text-5xl lg:text-7xl font-semibold text-pretty text-[#1f1f1f] hero-title dark:text-white'
+            className='text-3xl sm:text-5xl lg:text-7xl font-semibold text-pretty text-[#1f1f1f] hero-title dark:text-white'
           />
-          <p className='w-3/4 paragraph opacity-0 animate-[appear-element_150ms_ease-out_forwards] [animation-delay:0.8s]'>
+          <p className='w-full lg:w-3/4 paragraph text-balance opacity-0 animate-[appear-element_150ms_ease-out_forwards] [animation-delay:0.8s]'>
             {t('landing.subtitle')}
           </p>
         </header>
